30b445daac74e71092d186e568b087495c55c171
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-12-10 22:49:44 +0900

    20221210 First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "hiiragi-grpc-server"
---
+++ src/test/kotlin/net/konohana/sakuya/hiiragi/HiiragiGrpcServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.konohana.sakuya.hiiragi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HiiragiGrpcServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        println("TEST")
+    }
+}
